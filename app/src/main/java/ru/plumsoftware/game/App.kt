d2fd9918e0ee21f49b83c30da4569e276456ca99
Slava <plumsoftwareofficial@gmail.com>
2025-08-09 10:53:19 +0600

    Добавил рекламные идентификаторы для Huawei App Gallery.

--- app/src/main/java/ru/plumsoftware/game/App.kt
+++ app/src/main/java/ru/plumsoftware/game/App.kt
@@ -1,10 +1,10 @@
 package ru.plumsoftware.game
 
 import android.app.Application
-import ru.plumsoftware.game.ads.AdsRuStore
+import ru.plumsoftware.game.ads.AdsHuaweiAppGallery
 
 class App : Application() {
     companion object {
-        val adsBase = AdsRuStore()
+        val adsBase = AdsHuaweiAppGallery()
     }
 }
